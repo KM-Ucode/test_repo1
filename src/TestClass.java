4bc67efbae72ff76293efd9f8428cada85f1936e
KM-Ucode <148298124+KM-Ucode@users.noreply.github.com>
2023-10-31 22:17:38 -0400

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -4,5 +4,9 @@ public class TestClass {
         System.out.println("Hello Git and GitHub");
         System.out.println("Just practicing");
         System.out.println("Practicing from UI");
+
+        System.out.println("Hello Git and GitHub");
+        System.out.println("Just practicing");
+        System.out.println("Practicing from UI");
     }
 }
